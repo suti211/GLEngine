07d1279d08bd797001b3e930ae1d1a7ac4e75601
suti211 <suti211@gmail.com>
2014-11-02 18:08:51 +0100

    Control functions
    Our window is now black, and won't insta-close anymore

--- src/com/base/engine/MainComponent.java
+++ src/com/base/engine/MainComponent.java
@@ -3,13 +3,56 @@ package com.base.engine;
 public class MainComponent
 {
 
-	public static final int HEIGTH = 800;
-	public static final int WIDHT = 600;
+	public static final int HEIGTH = 600;
+	public static final int WIDHT = 800;
 	public static final String TITLE = "GLEngine_0.1a";
+
+	// maincomponent skeleton
+	public MainComponent()
+	{
+
+	}
+
+	// starts the engine ... haha
+	public void start()
+	{
+		run();
+	}
+
+	// stops the engine
+	public void stop()
+	{
+
+	}
+
+	// logic update function
+	public void run()
+	{
+		while (!Window.isCloseRequested())
+		{
+			render();
+		}
+	}
+
+	// render function
+	public void render()
+	{
+		Window.render();
+	}
+
+	// Cleanup after closing program
+	public void cleanUp()
+	{
+
+	}
 
 	public static void main(String[] args)
 	{
 		Window.createWindow(WIDHT, HEIGTH, TITLE);
+
+		MainComponent engine = new MainComponent();
+
+		engine.start();
 	}
 
 }
