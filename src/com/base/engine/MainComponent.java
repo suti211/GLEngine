ead36aac4bf678462f0d394456890b6118acfe97
suti211 <suti211@gmail.com>
2014-11-02 17:45:10 +0100

    MainComponent Constants, and Window
    Created constants for the heigth witd and title, due to easier
    management

--- src/com/base/engine/MainComponent.java
+++ src/com/base/engine/MainComponent.java
@@ -3,9 +3,13 @@ package com.base.engine;
 public class MainComponent
 {
 
+	public static final int HEIGTH = 800;
+	public static final int WIDHT = 600;
+	public static final String TITLE = "GLEngine_0.1a";
+
 	public static void main(String[] args)
 	{
-		System.out.println("Hello");
+		Window.createWindow(WIDHT, HEIGTH, TITLE);
 	}
 
 }
