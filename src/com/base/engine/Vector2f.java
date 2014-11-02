bc46e13571558730ee8c6f8cd80cd45fd88de772
suti211 <suti211@gmail.com>
2014-11-02 21:53:07 +0100

    Implemented all kinds of shit 2d vector math

--- src/com/base/engine/Vector2f.java
+++ src/com/base/engine/Vector2f.java
@@ -11,6 +11,78 @@ public class Vector2f
 		this.y = y;
 	}
 
+	public float length()
+	{
+		return (float) Math.sqrt(x * x + y * y);
+	}
+
+	public float dot(Vector2f r)
+	{
+		return x * r.getX() + y * r.getY();
+	}
+
+	public Vector2f normalize()
+	{
+		float length = length();
+
+		x /= length;
+		y /= length;
+
+		return this;
+	}
+
+	public Vector2f rotate(float angle)
+	{
+		double rad = Math.toRadians(angle);
+		double cos = Math.cos(rad);
+		double sin = Math.sin(rad);
+
+		return new Vector2f((float) (x * cos - y * sin), (float) (x * sin + y
+				* cos));
+	}
+
+	public Vector2f add(Vector2f r)
+	{
+		return new Vector2f(x + r.getX(), y + r.getY());
+	}
+
+	public Vector2f add(float r)
+	{
+		return new Vector2f(x - r, y - r);
+	}
+
+	public Vector2f subtract(Vector2f r)
+	{
+		return new Vector2f(x - r.getX(), y - r.getY());
+	}
+
+	public Vector2f subtract(float r)
+	{
+		return new Vector2f(x - r, y - r);
+	}
+
+	public Vector2f multiply(Vector2f r)
+	{
+		return new Vector2f(x * r.getX(), y * r.getY());
+	}
+
+	public Vector2f multiply(float r)
+	{
+		return new Vector2f(x * r, y * r);
+	}
+
+	public Vector2f divide(Vector2f r)
+	{
+		return new Vector2f(x / r.getX(), y / r.getY());
+	}
+
+	public Vector2f divide(float r)
+	{
+		return new Vector2f(x / r, y / r);
+	}
+
+	// ********************************************************
+
 	public String toString()
 	{
 		return "(" + x + " " + y + ")";
