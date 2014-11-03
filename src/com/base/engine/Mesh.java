3ee03feb7bd73e84a21e5d5fdb4220d01e47fa5d
suti211 <suti211@gmail.com>
2014-11-03 01:13:49 +0100

    So much Shit
    Opengl now draws a triangle, the assbanging now begins

--- src/com/base/engine/Mesh.java
+++ src/com/base/engine/Mesh.java
@@ -1,8 +1,16 @@
 package com.base.engine;
 
+import static org.lwjgl.opengl.GL11.GL_FLOAT;
+import static org.lwjgl.opengl.GL11.GL_TRIANGLES;
+import static org.lwjgl.opengl.GL11.glDrawArrays;
 import static org.lwjgl.opengl.GL15.GL_ARRAY_BUFFER;
+import static org.lwjgl.opengl.GL15.GL_STATIC_DRAW;
 import static org.lwjgl.opengl.GL15.glBindBuffer;
+import static org.lwjgl.opengl.GL15.glBufferData;
 import static org.lwjgl.opengl.GL15.glGenBuffers;
+import static org.lwjgl.opengl.GL20.glDisableVertexAttribArray;
+import static org.lwjgl.opengl.GL20.glEnableVertexAttribArray;
+import static org.lwjgl.opengl.GL20.glVertexAttribPointer;
 
 public class Mesh
 {
@@ -20,6 +28,19 @@ public class Mesh
 		size = vertices.length * Vertex.SIZE;
 
 		glBindBuffer(GL_ARRAY_BUFFER, vbo);
-		// glBufferData(GL_ARRAY_BUFFER, data, usage);
+		glBufferData(GL_ARRAY_BUFFER, Util.createFlippedBuffer(vertices),
+				GL_STATIC_DRAW);
+	}
+
+	public void draw()
+	{
+		glEnableVertexAttribArray(0);
+
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		glVertexAttribPointer(0, 3, GL_FLOAT, false, Vertex.SIZE * 4, 0);
+
+		glDrawArrays(GL_TRIANGLES, 0, size);
+
+		glDisableVertexAttribArray(0);
 	}
 }
